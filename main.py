601a82a5c78f25dbe34beb018d86d5d8cfa13024
Pritamgadget <pritamgadget@gmail.com>
2022-11-21 22:41:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -466,6 +466,8 @@ async def say(ctx, *, text):
         await message.delete()
 
         await ctx.send(f"{text}")
+    elif ctx.message.author.id == 856036736970260490:
+	await ctx.send(f"{ctx.message.author.mention} You're yet to inherit me)"
     elif text == "Only Tam is allowed to use it noob.":
         await ctx.send(f"{ctx.message.author.mention} stfu")
     else:
